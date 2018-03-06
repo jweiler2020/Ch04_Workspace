77a36c578f1c94cd8b13674df43e0ce23445e381
jweiler2020 <jweiler2020@cusd.claremont.edu>
2018-03-06 13:16:44 -0800

    Finished Number 8

--- src/PrintTriangleStars.java
+++ src/PrintTriangleStars.java
@@ -12,11 +12,26 @@ public class PrintTriangleStars
 
 	public static void printTriangle(int n)
 	{
+		printTriangleRecursive(n, n-1);
+	}
 
+	private static void printTriangleRecursive(int n, int sub)
+	{
+		if(sub < 0)
+			return;
+		printStars(n - sub);
+		System.out.println();
+		printTriangleRecursive(n, sub-1);
 	}
 
 	public static void main(String[] args)
 	{
-		printTriangle(5);
+		Scanner kb = new Scanner(System.in);
+		System.out.print("Enter the amount of rows in the triangle: ");
+		int rows = kb.nextInt();
+		kb.nextLine();
+
+		System.out.println("Here you go:");
+		printTriangle(rows);
 	}
 }
